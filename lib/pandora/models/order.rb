f6a182bb7ce7af854773562e7d38892b340b67a7
yangli85 <yangli@CNyangli.local>
2016-04-01 01:15:58 +0800

    add attrites to order -yang

--- lib/pandora/models/order.rb
+++ lib/pandora/models/order.rb
@@ -6,6 +6,15 @@ module Pandora
       enum products: [:VIP, :STAR]
       validates :product, inclusion: {in: products.keys}
       has_one :payment_log
+
+      def attributes
+        {
+            id: product,
+            count: count,
+            status: status,
+            result: result
+        }
+      end
     end
   end
 end
