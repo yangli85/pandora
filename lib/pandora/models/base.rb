dfe56c72e7c3ced8d9a3b37408bbd3dcc6c99001
yangli85 <yang.li@rea-group.com>
2016-05-29 16:44:51 +0800

    change timezone

--- lib/pandora/models/base.rb
+++ lib/pandora/models/base.rb
@@ -4,10 +4,10 @@ module Pandora
   module Models
     class Base < ::ActiveRecord::Base
       self.abstract_class = true
+      self.default_timezone = :local
       dbconfig = YAML::load(ERB.new(File.read(File.expand_path('../db/database.yml', File.dirname(__FILE__)))).result)
       env = ENV["RACK_ENV"] || "development"
       establish_connection dbconfig[env]
-
     end
   end
 end
