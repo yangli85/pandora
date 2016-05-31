96552a1ea6a4b6de71067c1c531c8f27a21bc3b0
yangli85 <yang.li@rea-group.com>
2016-05-31 16:50:39 +0800

    fix test issue

---
+++ lib/pandora/db/migrate/20160602153123_add_size_to_images.rb
@@ -0,0 +1,11 @@
+class AddSizeToImages < ActiveRecord::Migration
+  def up
+    add_column :images, :width, :integer
+    add_column :images, :height, :integer
+  end
+
+  def down
+    remove_column :images, :width
+    remove_column :images, :height
+  end
+end
